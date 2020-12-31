f23031bc0b5a69d8856b0ced3cc8fea8bcfbc2cb
Klemen Hribar <klemen.hribar@hpe.com>
2020-12-31 12:43:06 +0100

    Step 1 - 3. forEach and Filter

---
+++ src/main/java/programming/FP01.java
@@ -0,0 +1,60 @@
+package programming;
+
+import java.util.List;
+
+public class FP01 {
+
+    public static void main(String[] args){
+
+        List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+
+        printEvenNumbersInListFunctional(
+                numbers
+
+        );
+    }
+
+    private static void printAllNumbersInListStructured(List<Integer> numbers) {
+        //How to loop the numbers?
+        for (int number: numbers) {
+            System.out.println(number);
+        }
+
+    }
+    /*
+    private static void print(int number){
+        System.out.println(number);
+    }
+
+     */
+
+    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+        //What to do?
+        numbers.stream()
+                .forEach(System.out::println);//Method reference
+
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+        //How to loop the numbers?
+        for (int number: numbers) {
+            if(number%2==0) {
+                System.out.println(number);
+            }
+        }
+
+    }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        //What to do?
+        numbers.stream()
+                //Filter - Only Allow Even Numbers
+                .filter(FP01::isEven)
+                .forEach(System.out::println);//Method reference
+
+    }
+
+    private static boolean isEven(int number){
+        return number%2 == 0;
+    }
+}
